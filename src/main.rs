38035e1231ef9e65d8417618fc42c0368360f68b
Yishen Miao <mys721tx@gmail.com>
2020-06-21 14:16:20 -0700

    Implementing a filesystem that does nothing
    Implements a filesystem that does nothing.

---
+++ src/main.rs
@@ -0,0 +1,258 @@
+use std::ffi::OsStr;
+use std::path::Path;
+
+use clap::{app_from_crate, crate_authors, crate_description, crate_name, crate_version, Arg};
+
+use fuse_mt::*;
+use libc::*;
+use time::*;
+
+pub struct NullFS {}
+
+impl FilesystemMT for NullFS {
+    fn init(&self, _req: RequestInfo) -> ResultEmpty {
+        dbg!("init");
+        Ok(())
+    }
+
+    fn destroy(&self, _req: RequestInfo) {
+    }
+
+    fn getattr(&self, _req: RequestInfo, _path: &Path, _fh: Option<u64>) -> ResultEntry {
+        Err(ENOENT)
+    }
+
+    fn chmod(&self, _req: RequestInfo, _path: &Path, _fh: Option<u64>, _mode: u32) -> ResultEmpty {
+        Err(ENOENT)
+    }
+
+    fn chown(
+        &self,
+        _req: RequestInfo,
+        _path: &Path,
+        _fh: Option<u64>,
+        _uid: Option<u32>,
+        _gid: Option<u32>,
+    ) -> ResultEmpty {
+        Err(ENOENT)
+    }
+
+    fn truncate(
+        &self,
+        _req: RequestInfo,
+        _path: &Path,
+        _fh: Option<u64>,
+        _size: u64,
+    ) -> ResultEmpty {
+        Err(ENOENT)
+    }
+
+    fn utimens(
+        &self,
+        _req: RequestInfo,
+        _path: &Path,
+        _fh: Option<u64>,
+        _atime: Option<Timespec>,
+        _mtime: Option<Timespec>,
+    ) -> ResultEmpty {
+        Err(ENOENT)
+    }
+
+    fn utimens_macos(
+        &self,
+        _req: RequestInfo,
+        _path: &Path,
+        _fh: Option<u64>,
+        _crtime: Option<Timespec>,
+        _chgtime: Option<Timespec>,
+        _bkuptime: Option<Timespec>,
+        _flags: Option<u32>,
+    ) -> ResultEmpty {
+        Err(ENOENT)
+    }
+
+    fn readlink(&self, _req: RequestInfo, _path: &Path) -> ResultData {
+        Err(ENOENT)
+    }
+
+    fn mknod(
+        &self,
+        _req: RequestInfo,
+        _parent: &Path,
+        _name: &OsStr,
+        _mode: u32,
+        _rdev: u32,
+    ) -> ResultEntry {
+        Err(ENOENT)
+    }
+
+    fn mkdir(&self, _req: RequestInfo, _parent: &Path, _name: &OsStr, _mode: u32) -> ResultEntry {
+        Err(ENOENT)
+    }
+
+    fn unlink(&self, _req: RequestInfo, _parent: &Path, _name: &OsStr) -> ResultEmpty {
+        Err(ENOENT)
+    }
+
+    fn rmdir(&self, _req: RequestInfo, _parent: &Path, _name: &OsStr) -> ResultEmpty {
+        Err(ENOENT)
+    }
+
+    fn symlink(
+        &self,
+        _req: RequestInfo,
+        _parent: &Path,
+        _name: &OsStr,
+        _target: &Path,
+    ) -> ResultEntry {
+        Err(ENOENT)
+    }
+
+    fn rename(
+        &self,
+        _req: RequestInfo,
+        _parent: &Path,
+        _name: &OsStr,
+        _newparent: &Path,
+        _newname: &OsStr,
+    ) -> ResultEmpty {
+        Err(ENOENT)
+    }
+
+    fn link(
+        &self,
+        _req: RequestInfo,
+        _path: &Path,
+        _newparent: &Path,
+        _newname: &OsStr,
+    ) -> ResultEntry {
+        Err(ENOENT)
+    }
+
+    fn open(&self, _req: RequestInfo, _path: &Path, _flags: u32) -> ResultOpen {
+        Err(ENOENT)
+    }
+
+    fn read(
+        &self,
+        _req: RequestInfo,
+        _path: &Path,
+        _fh: u64,
+        _offset: u64,
+        _size: u32,
+        _result: impl FnOnce(Result<&[u8], c_int>),
+    ) {
+    }
+
+    fn write(
+        &self,
+        _req: RequestInfo,
+        _path: &Path,
+        _fh: u64,
+        _offset: u64,
+        _data: Vec<u8>,
+        _flags: u32,
+    ) -> ResultWrite {
+        Err(ENOENT)
+    }
+
+    fn flush(&self, _req: RequestInfo, _path: &Path, _fh: u64, _lock_owner: u64) -> ResultEmpty {
+        Err(ENOENT)
+    }
+
+    fn release(
+        &self,
+        _req: RequestInfo,
+        _path: &Path,
+        _fh: u64,
+        _flags: u32,
+        _lock_owner: u64,
+        _flush: bool,
+    ) -> ResultEmpty {
+        Err(ENOENT)
+    }
+
+    fn fsync(&self, _req: RequestInfo, _path: &Path, _fh: u64, _datasync: bool) -> ResultEmpty {
+        Err(ENOENT)
+    }
+
+    fn opendir(&self, _req: RequestInfo, _path: &Path, _flags: u32) -> ResultOpen {
+        Err(ENOENT)
+    }
+
+    fn readdir(&self, _req: RequestInfo, _path: &Path, _fh: u64) -> ResultReaddir {
+        Err(ENOENT)
+    }
+
+    fn releasedir(&self, _req: RequestInfo, _path: &Path, _fh: u64, _flags: u32) -> ResultEmpty {
+        Err(ENOENT)
+    }
+
+    fn fsyncdir(&self, _req: RequestInfo, _path: &Path, _fh: u64, _datasync: bool) -> ResultEmpty {
+        Err(ENOENT)
+    }
+
+    fn statfs(&self, _req: RequestInfo, _path: &Path) -> ResultStatfs {
+        Err(ENOENT)
+    }
+
+    fn setxattr(
+        &self,
+        _req: RequestInfo,
+        _path: &Path,
+        _name: &OsStr,
+        _value: &[u8],
+        _flags: u32,
+        _position: u32,
+    ) -> ResultEmpty {
+        Err(ENOENT)
+    }
+
+    fn getxattr(&self, _req: RequestInfo, _path: &Path, _name: &OsStr, _size: u32) -> ResultXattr {
+        Err(ENOENT)
+    }
+
+    fn listxattr(&self, _req: RequestInfo, _path: &Path, _size: u32) -> ResultXattr {
+        Err(ENOENT)
+    }
+
+    fn removexattr(&self, _req: RequestInfo, _path: &Path, _name: &OsStr) -> ResultEmpty {
+        Err(ENOENT)
+    }
+
+    fn access(&self, _req: RequestInfo, _path: &Path, _mask: u32) -> ResultEmpty {
+        Err(ENOENT)
+    }
+
+    fn create(
+        &self,
+        _req: RequestInfo,
+        _parent: &Path,
+        _name: &OsStr,
+        _mode: u32,
+        _flags: u32,
+    ) -> ResultCreate {
+        Err(ENOENT)
+    }
+}
+
+fn main() {
+    let matches = app_from_crate!()
+        .arg(
+            Arg::with_name("MOUNT")
+                .help("path to the mounting point")
+                .index(1)
+                .required(true),
+        )
+        .get_matches();
+
+    let path = Path::new(matches.value_of("MOUNT").unwrap());
+
+    dbg!(path);
+
+    let filesystem = NullFS {};
+
+    let fuse_args: Vec<&OsStr> = vec![&OsStr::new("-o"), &OsStr::new("auto_unmount")];
+
+    fuse_mt::mount(fuse_mt::FuseMT::new(filesystem, 1), &path, &fuse_args).unwrap();
+}
